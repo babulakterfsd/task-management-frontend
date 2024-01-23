8a5410e7b974c0351d55cd6271fecf4f831951db
Babul Akter <babulakterfsd@gmail.com>
2024-01-23 17:34:03 +0600

    redux persist added

--- src/redux/features/authSlice.ts
+++ src/redux/features/authSlice.ts
@@ -1,5 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { authInitialState } from '../initialStates';
+import { RootState } from '../store';
 
 const authSlice = createSlice({
   name: 'auth',
@@ -19,3 +20,6 @@ const authSlice = createSlice({
 export const { setUserInLocalState, RemoveUserFromLocalState } =
   authSlice.actions;
 export default authSlice.reducer;
+
+export const useCurrentToken = (state: RootState) => state.auth.token;
+export const useCurrentUser = (state: RootState) => state.auth.user;
--- src/routes/All.routes.tsx
+++ src/routes/All.routes.tsx
@@ -4,6 +4,7 @@ import NotFound from '@/pages/NotFound';
 import { createBrowserRouter } from 'react-router-dom';
 import { dashboardRoutePaths } from './Dashboard.routes';
 import { mainRoutePaths } from './Main.routes';
+import ProtectedRoute from './ProtectedRoute';
 
 const router = createBrowserRouter([
   {
@@ -13,7 +14,11 @@ const router = createBrowserRouter([
   },
   {
     path: '/dashboard',
-    element: <DashboardLayout />,
+    element: (
+      <ProtectedRoute>
+        <DashboardLayout />
+      </ProtectedRoute>
+    ),
     children: dashboardRoutePaths,
   },
   {
