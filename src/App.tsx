bd5e72ea441682b7355d1ea1708166a08312b3a1
Babul Akter <babulakterfsd@gmail.com>
2024-01-23 16:51:45 +0600

    redux persist added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
-import { store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './redux/store';
 import router from './routes/All.routes';
 import './styles/index.css';
 
 function App() {
   return (
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <PersistGate loading={null} persistor={persistor}>
+        <RouterProvider router={router} />
+      </PersistGate>
     </Provider>
   );
 }
