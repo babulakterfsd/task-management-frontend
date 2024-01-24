aa0b0f8f9317959582e6d794912ebc9cd41085f6
Babul Akter <babulakterfsd@gmail.com>
2024-01-24 20:22:33 +0600

    role based todo fetching implemented

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,4 +1,15 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import {
+  BaseQueryApi,
+  BaseQueryFn,
+  DefinitionType,
+  FetchArgs,
+  createApi,
+  fetchBaseQuery,
+} from '@reduxjs/toolkit/query/react';
+import {
+  RemoveUserFromLocalState,
+  setUserInLocalState,
+} from '../features/authSlice';
 import { RootState } from '../store';
 
 const baseQuery = fetchBaseQuery({
@@ -15,9 +26,45 @@ const baseQuery = fetchBaseQuery({
   },
 });
 
+const baseQueryWithRefreshToken: BaseQueryFn<
+  FetchArgs,
+  BaseQueryApi,
+  DefinitionType
+> = async (args, api, extraOptions): Promise<any> => {
+  let result = await baseQuery(args, api, extraOptions);
+
+  if (result?.error?.status === 401) {
+    const response = await fetch(
+      'http://localhost:5000/api/auth/refresh-token',
+      {
+        method: 'POST',
+        credentials: 'include',
+      }
+    );
+
+    const data = await response.json();
+    if (data?.data?.accessToken) {
+      const user = (api.getState() as RootState).auth.user;
+
+      api.dispatch(
+        setUserInLocalState({
+          user,
+          token: data?.data?.accessToken,
+        })
+      );
+
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      api.dispatch(RemoveUserFromLocalState());
+    }
+  }
+
+  return result;
+};
+
 export const baseApi = createApi({
   reducerPath: 'baseApi',
-  baseQuery: baseQuery,
+  baseQuery: baseQueryWithRefreshToken,
   tagTypes: ['Todo'],
   endpoints: () => ({}),
 });
